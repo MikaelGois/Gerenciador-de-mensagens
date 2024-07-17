31698cdcb79887cd14c7c0cccf65a67ce50b790e
TalysonSilva <talyson.rodigues@gmail.com>
2024-07-17 17:38:52 -0300

    feat: Projeto inicial

---
+++ src/main/java/desktop/application/gerenciador_de_mensagens/GerenciadorDeMensagensApplication.java
@@ -0,0 +1,13 @@
+package desktop.application.gerenciador_de_mensagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeMensagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeMensagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desktop/application/gerenciador_de_mensagens/GerenciadorDeMensagensApplicationTests.java
@@ -0,0 +1,13 @@
+package desktop.application.gerenciador_de_mensagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeMensagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
