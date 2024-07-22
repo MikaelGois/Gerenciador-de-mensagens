d2263bb1909dd1427fd4f55ec4764b66d3f2b645
MikaelGois01 <mikael_gois1@hotmail.com>
2024-07-22 14:50:05 -0300

    modified:   index.html 	new file:   release.md 	modified:   script.js 	modified:   styles.css

---
+++ release.md
@@ -0,0 +1,92 @@
+# Notas de atualização
+
+Todas as alterações significativas neste projeto serão documentadas neste arquivo.
+
+O formato é baseado em [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+e este projeto adere ao [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased] - Não lançado
+
+### Added - Adicionado
+
+- Notas de rodapé com:
+  - Autor,
+  - Cidade,
+  - Ano,
+  - LinkedIn,
+  - Link para as notas de atualização.
+- Botão de marcar todas as mensagens.
+- Botão de desmarcar todas as mensagens.
+- Barra de pesquisa.
+- Tratativa de erros.
+
+### Fixed - Corrigido
+
+- Botão de excluir mensagens selecionadas não abre o popup de confirmação.
+- Páginação não está funcionando.
+- Responsividade em telas menores.
+- Botão de editar mensagens parou de funcionar. [v1.2.5ac Beta]
+
+### Changed - Alterado
+
+- Estilo de caixa de seleção de mensagens.
+- Estilo de popup de edição de mensagens.
+
+### Deprecated - Descontinuado
+
+- Nenhuma funcionalidade foi descontinuada nessa versão.
+
+### Removed - Removido
+
+- Nenhuma funcionalidade foi removida nessa versão.
+
+## [1.2.5-ac] [beta] - 2024-07-XX
+
+### Added - Adicionado
+
+- Nenhuma funcionalidade foi adicionada nessa versão.
+
+### Fixed - Corrigido
+
+- Botão de excluir mensagens selecionadas não abre o popup de confirmação.
+- Botão de importar mensagens não funciona. [Corrigido]
+- Filtro de mensagens não está funcionando. [Corrigido]
+- Excluir mensgem não exclui as mensagens correspondentes. [Corrigido]
+
+### Changed - Alterado
+
+- Exibição da mensagem "Mensagens importadas" ao importar mensagem foi modificada para "Mensagens importadas com sucesso!".
+- Exibição da mensagem "Mensagens exportadas" ao importar mensagem foi modificada para "Mensagens exportadas com sucesso!".
+- Exibição da mensagem "Mensagens selecionadas exportadas" ao importar mensagem foi modificada para "Mensagens selecionadas exportadas com sucesso!".
+
+### Deprecated - Descontinuado
+
+- Páginação: O recurso de páginação está com problemas ao lidar com as mensagens exibidas, causando comportamentos indesejados ao excluir mensagens e filtrar mensagens por mais recentes, mais usadas, etc.
+
+### Removed - Removido
+
+- Nenhuma funcionalidade foi removida nessa versão.
+
+## [1.2.5-ab] [beta] - 2024-07-08
+
+### Added - Adicionado
+
+- Arquivo `README.md` com a descrição do projeto.
+
+### Fixed - Corrigido
+
+- Clicar fora do menu de contexto não fecha o menu quando editPopup ou deletePopup não está com valor "none" - corrigido nessa versão.
+- Espaçamento dos botões no modo selecionar está quebrado - corrigido nessa versão.
+
+### Changed - Alterado
+
+- Nenhuma funcionalidade foi alterada nessa versão.
+
+### Deprecated - Descontinuado
+
+- Nenhuma funcionalidade foi descontinuada nessa versão.
+
+### Removed - Removido
+
+- Nenhuma funcionalidade foi removida nessa versão.
--- script.js
+++ script.js
@@ -1,26 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const messageInput = document.getElementById('message-input');
   const addButton = document.getElementById('add-button');
-  const sortSelect = document.getElementById('sort-select');
+  const messageInput = document.getElementById('message-input');
+
   const column1 = document.getElementById('column1');
   const column2 = document.getElementById('column2');
+
   const toggleDarkMode = document.getElementById('toggle-dark-mode');
+
   const notification = document.getElementById('notification');
-  const importFile = document.getElementById('import-file');
-  const importButton = document.getElementById('import-button');
-  const exportButton = document.getElementById('export-button');
-  const popup = document.getElementById('popup');
+
+  const deleteOption = document.getElementById('delete-option');
+  const deletePopup = document.getElementById('delete-popup');
   const confirmDelete = document.getElementById('confirm-delete');
   const cancelDelete = document.getElementById('cancel-delete');
 
+  const editOption = document.getElementById('edit-option');
+  const editPopup = document.getElementById('edit-popup');
+  const editMessageInput = document.getElementById('edit-message-input');
+  const confirmEdit = document.getElementById('confirm-edit');
+  const cancelEdit = document.getElementById('cancel-edit');
+
+  const contextMenu = document.getElementById('context-menu');
+  const sortSelect = document.getElementById('sort-select');
+
+
+  const importFile = document.getElementById('import-file');
+  const selectButton = document.getElementById('select-button');
+  const importButton = document.getElementById('import-button');
+  const exportButton = document.getElementById('export-button');
+  const deleteSelectedButton = document.getElementById('delete-selected-button');
+
+  const prevPage = document.getElementById('prev-page');
+  const nextPage = document.getElementById('next-page');
+
   let messages = JSON.parse(localStorage.getItem('messages')) || [];
   let darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+
   let messageToDeleteIndex = null;
+  let messageToEditIndex = null;
+
+  let currentPage = 1;
+  const messagesPerPage = 10;
+
+  let selectMode = false;
+  let selectedMessages = new Set();
 
   const saveMessages = () => {
     localStorage.setItem('messages', JSON.stringify(messages));
   };
 
+  // Adiciona mensagem - Funcionando
+  addButton.addEventListener('click', () => {
+    const messageText = messageInput.value.trim();
+    if (messageText) {
+      const newMessage = {
+        text: messageText,
+        dateAdded: new Date().toISOString(),
+        lastUsed: new Date().toISOString(),
+        usageCount: 0,
+      };
+      messages.push(newMessage);
+      saveMessages();
+      messageInput.value = '';
+      renderMessages();
+      showNotification('Mensagem adicionada com sucesso');
+    }
+  });
+
+  // Notificação de confirmação
   const showNotification = (message) => {
     notification.textContent = message;
     notification.style.display = 'block';
@@ -29,6 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 3000);
   };
 
+  // Mostra mensagens
   const renderMessages = () => {
     column1.innerHTML = '';
     column2.innerHTML = '';
@@ -45,54 +93,211 @@ document.addEventListener('DOMContentLoaded', () => {
       sortedMessages.sort((a, b) => new Date(b.dateAdded) - new Date(a.dateAdded));
     }
 
+    // const start = (currentPage - 1) * messagesPerPage;
+    // const end = start + messagesPerPage;
+    // const paginatedMessages = sortedMessages.slice(start, end);
+
     sortedMessages.forEach((message, index) => {
+      // paginatedMessages.forEach((message, index) => {
       const messageBox = document.createElement('div');
       messageBox.classList.add('message-box');
       messageBox.textContent = message.text;
-      messageBox.addEventListener('click', () => {
+      if (selectMode) {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('message-checkbox');
+        checkbox.checked = selectedMessages.has(start + index);
+        checkbox.addEventListener('change', () => {
+          if (checkbox.checked) {
+            selectedMessages.add(start + index);
+          } else {
+            selectedMessages.delete(start + index);
+          }
+          updateButtons();
+        });
+        messageBox.appendChild(checkbox);
+        messageBox.addEventListener('click', (e) => {
+          if (e.target !== checkbox) {
+            checkbox.checked = !checkbox.checked;
+            checkbox.dispatchEvent(new Event('change'));
+          }
+        });
+      } else {
         // se estiver fora do modo de seleção, copia a mensagem para a área de transferência
-        navigator.clipboard.writeText(message.text);
-        message.usageCount += 1;
-        message.lastUsed = new Date().toISOString();
-        saveMessages();
-        showNotification('Mensagem copiada para a área de transferência');
-        renderMessages();
-      });
+        messageBox.addEventListener('click', () => {
+          navigator.clipboard.writeText(message.text);
+          message.usageCount += 1;
+          message.lastUsed = new Date().toISOString();
+          saveMessages();
+          showNotification('Mensagem copiada para a área de transferência');
+          renderMessages();
+        });
+      }
+      // Adiciona um menu de contexto ao clicar com o botão direito do mouse - Funcionando
       messageBox.addEventListener('contextmenu', (e) => {
-        // se clicar com o botão direito do mouse, abre o popup de exclusão
-        e.preventDefault();
-        messageToDeleteIndex = index;
-        document.body.classList.add('blur-background');
-        popup.classList.add('show');
-        popup.style.display = 'block';
+        if (!selectMode) {
+          // Apresenta o menu de contexto apenas se o modo de seleção não estiver ativado
+          e.preventDefault();
+          // messageToEditIndex = start + index;
+          // messageToDeleteIndex = start + index;
+          contextMenu.style.top = `${e.clientY}px`;
+          contextMenu.style.left = `${e.clientX}px`;
+          contextMenu.style.display = 'block';
+          contextMenu.classList.add('show');
+          contextMenu.dataset.index = start + index;
+          document.body.classList.add('blur-background');
+
+          // contextMenu.addEventListener('click', () => {
+          //     // Ao clicar em "Editar" - Quebrado
+          //     confirmEdit.addEventListener('click', () => {
+          //         if (messageToEditIndex !== null) {
+          //             messages[messageToEditIndex].text = document.getElementById('edit-message-input').value;
+          //             saveMessages();
+          //             renderMessages();
+          //             showNotification('Mensagem editada');
+          //             document.body.classList.remove('blur-background');
+          //             editPopup.classList.remove('show');
+          //             editPopup.style.display = 'none';
+          //             messageToEditIndex = null;
+          //         }
+          //     });
+
+          //     cancelEdit.addEventListener('click', () => {
+          //         document.body.classList.remove('blur-background');
+          //         editPopup.classList.remove('show');
+          //         editPopup.style.display = 'none';
+          //         messageToEditIndex = null;
+          //     });
+
+          //     editOption.addEventListener('click', () => {
+          //         messageToEditIndex = contextMenu.dataset.index;
+          //         document.getElementById('edit-message-input').value = messages[messageToEditIndex].text;
+          //         document.body.classList.add('blur-background');
+          //         editPopup.classList.add('show');
+          //         editPopup.style.display = 'block';
+          //         contextMenu.style.display = 'none';
+          //     });
+
+          //     // Ao clicar em "Deletar" - Quebrado
+          //     confirmDelete.addEventListener('click', () => {
+          //         if (messageToDeleteIndex !== null) {
+          //             messages.splice(messageToDeleteIndex, 1);
+          //             saveMessages();
+          //             renderMessages();
+          //             showNotification('Mensagem excluída');
+          //             document.body.classList.remove('blur-background');
+          //             deletePopup.classList.remove('show');
+          //             deletePopup.style.display = 'none';
+          //             messageToDeleteIndex = null;
+          //         }
+          //     });
+
+          //     cancelDelete.addEventListener('click', () => {
+          //         document.body.classList.remove('blur-background');
+          //         deletePopup.classList.remove('show');
+          //         deletePopup.style.display = 'none';
+          //         messageToDeleteIndex = null;
+          //     });
+          // });
+
+          // e.preventDefault();
+          // messageToEditIndex = start + index;
+          // contextMenu.style.top = `${e.clientY}px`;
+          // contextMenu.style.left = `${e.clientX}px`;
+          // contextMenu.style.display = 'block';
+          // document.body.classList.add('blur-background');
+        }
       });
+
       if (index % 2 === 0) {
         column1.appendChild(messageBox);
       } else {
         column2.appendChild(messageBox);
       }
     });
+
+    updateButtons();
   };
 
-  addButton.addEventListener('click', () => {
-    const messageText = messageInput.value.trim();
-    if (messageText) {
-      const newMessage = {
-        text: messageText,
-        dateAdded: new Date().toISOString(),
-        lastUsed: new Date().toISOString(),
-        usageCount: 0
-      };
-      messages.push(newMessage);
+  // --- Inicio do menu de contexto - Parcialmente funcional
+
+  // Ao clicar em "Editar" - Parcialmente funcional
+  confirmEdit.addEventListener('click', () => {
+    if (messageToEditIndex !== null) {
+      messages[messageToEditIndex].text = document.getElementById('edit-message-input').value;
       saveMessages();
       renderMessages();
-      showNotification('Mensagem adicionada');
-      messageInput.value = '';
+      showNotification('Mensagem editada');
+      document.body.classList.remove('blur-background');
+      editPopup.classList.remove('show');
+      editPopup.style.display = 'none';
+      messageToEditIndex = null;
     }
   });
 
-  sortSelect.addEventListener('change', renderMessages);
+  cancelEdit.addEventListener('click', () => {
+    document.body.classList.remove('blur-background');
+    editPopup.classList.remove('show');
+    editPopup.style.display = 'none';
+    messageToEditIndex = null;
+  });
 
+  editOption.addEventListener('click', () => {
+    messageToEditIndex = contextMenu.dataset.index;
+    document.getElementById('edit-message-input').value = messages[messageToEditIndex].text;
+    document.body.classList.add('blur-background');
+    editPopup.classList.add('show');
+    editPopup.style.display = 'block';
+    contextMenu.style.display = 'none';
+  });
+
+  // Ao clicar em "Deletar" - Funcionando
+  confirmDelete.addEventListener('click', () => {
+    if (messageToDeleteIndex !== null) {
+      messages.splice(messageToDeleteIndex, 1);
+      saveMessages();
+      renderMessages();
+      showNotification('Mensagem excluída');
+      document.body.classList.remove('blur-background');
+      deletePopup.classList.remove('show');
+      deletePopup.style.display = 'none';
+      messageToDeleteIndex = null;
+    }
+  });
+
+  cancelDelete.addEventListener('click', () => {
+    document.body.classList.remove('blur-background');
+    deletePopup.classList.remove('show');
+    deletePopup.style.display = 'none';
+    messageToDeleteIndex = null;
+  });
+
+  deleteOption.addEventListener('click', () => {
+    messageToDeleteIndex = contextMenu.dataset.index;
+    document.body.classList.add('blur-background');
+    deletePopup.classList.add('show');
+    deletePopup.style.display = 'block';
+    contextMenu.style.display = 'none';
+  });
+
+  // Cancela ao clicar fora do menu de contexto - Funcionando
+  document.addEventListener('click', (e) => {
+    if (contextMenu.style.display !== 'none') {
+      if (contextMenu.classList.contains('show') && !contextMenu.contains(e.target)) {
+        document.body.classList.remove('blur-background');
+        contextMenu.style.display = 'none';
+        contextMenu.classList.remove('show');
+        messageToEditIndex = null;
+        messageToDeleteIndex = null;
+      }
+    };
+  });
+
+  // --- Fim do menu de contexto
+
+  // --- Inicio modo escuro
+
+  // Modo escuro - Funcionando
   toggleDarkMode.addEventListener('click', () => {
     darkMode = !darkMode;
     document.body.classList.toggle('dark-mode', darkMode);
@@ -107,6 +312,75 @@ document.addEventListener('DOMContentLoaded', () => {
     toggleDarkMode.textContent = 'Modo Claro';
   }
 
+  // --- Fim modo escuro
+
+
+  // Habilita ou desabilita os botões de acordo com o modo de seleção - Funcionando
+  const updateButtons = () => {
+    if (selectMode) {
+      importButton.disabled = true; // Desabilita o botão de importação
+      exportButton.disabled = selectedMessages.size === 0; // Desabilita o botão de exportação se nenhuma mensagem estiver selecionada
+      deleteSelectedButton.style.display = selectedMessages.size > 0 ? 'block' : 'none'; // Exibe o botão de exclusão se houver mensagens selecionadas
+    } else {
+      importButton.disabled = false; // Habilita o botão de importação
+      exportButton.disabled = false; // Habilita o botão de exportação
+      deleteSelectedButton.style.display = 'none'; // Oculta o botão de exclusão
+    }
+  };
+
+  // Entra ou sai do modo de seleção - Funcionando
+  const toggleSelectMode = () => {
+    selectMode = !selectMode;
+    selectedMessages.clear();
+    selectButton.textContent = selectMode ? 'Cancelar Seleção' : 'Selecionar';
+    renderMessages();
+  };
+
+  // Deleta mensagens selecionadas - Parcialmente funcional
+  const deleteSelectedMessages = () => {
+    messages = messages.filter((_, index) => !selectedMessages.has(index));
+    selectedMessages.clear();
+    saveMessages();
+    renderMessages();
+    showNotification('Mensagens deletadas com sucesso');
+    // Deve sair do modo de seleção
+  };
+
+  // Exporta mensagens - Parcialmente funcional
+  const exportMessages = () => {
+    if (selectMode) {
+      // Exporta apenas as mensagens selecionadas - Funcionando
+      selected = messages.filter((_, index) => selectedMessages.has(index));
+      // const blob = new Blob([JSON.stringify(selected)], { type: 'application/json' });
+      const content = selected.map(msg => `"${msg.text}"`).join('\n');
+      const blob = new Blob([content], { type: 'text/plain' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'mensagens_selecionadas.txt';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+      showNotification('Mensagens selecionadas exportadas com sucesso!');
+      // Deve sair do modo de seleção
+    } else {
+      // Exporta todas as mensagens - Funcionando
+      const content = messages.map(msg => `"${msg.text}"`).join('\n');
+      const blob = new Blob([content], { type: 'text/plain' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'mensagens.txt';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+      showNotification('Mensagens exportadas com sucesso!');
+    }
+  };
+
+  // Importa mensagens - Funcionando
   const importMessages = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -125,7 +399,7 @@ document.addEventListener('DOMContentLoaded', () => {
         messages = messages.concat(importedMessages);
         saveMessages();
         renderMessages();
-        showNotification('Mensagens importadas');
+        showNotification('Mensagens importadas com sucesso!');
       };
       reader.readAsText(file);
     }
@@ -133,49 +407,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   importFile.addEventListener('change', importMessages);
   importButton.addEventListener('click', () => importFile.click());
+  exportButton.addEventListener('click', exportMessages);
+  selectButton.addEventListener('click', toggleSelectMode);
+  deleteSelectedButton.addEventListener('click', deleteSelectedMessages);
 
-  exportButton.addEventListener('click', () => {
-    const content = messages.map(msg => `"${msg.text}"`).join('\n');
-    const blob = new Blob([content], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'mensagens.txt';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-    showNotification('Mensagens exportadas');
-  });
-
-  confirmDelete.addEventListener('click', () => {
-    if (messageToDeleteIndex !== null) {
-      messages.splice(messageToDeleteIndex, 1);
-      saveMessages();
-      renderMessages();
-      showNotification('Mensagem excluída');
-      document.body.classList.remove('blur-background');
-      popup.classList.remove('show');
-      popup.style.display = 'none';
-      messageToDeleteIndex = null;
-    }
-  });
-
-  cancelDelete.addEventListener('click', () => {
-    document.body.classList.remove('blur-background');
-    popup.classList.remove('show');
-    popup.style.display = 'none';
-    messageToDeleteIndex = null;
-  });
-
-  document.addEventListener('click', (e) => {
-    if (popup.classList.contains('show') && !popup.contains(e.target)) {
-      document.body.classList.remove('blur-background');
-      popup.classList.remove('show');
-      popup.style.display = 'none';
-      messageToDeleteIndex = null;
-    }
-  });
+  // Re-organiza a exibição das mensagens com base no filtro
+  sortSelect.addEventListener('change', renderMessages);
 
   renderMessages();
 });
